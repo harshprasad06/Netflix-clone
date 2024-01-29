e90236ed22e9bbea33a542b530e87b7f8eada864
harshprasad06 <khatriharsh508@gmail.com>
2024-01-30 00:52:23 +0530

    update sdk

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Amplify from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
+// import Amplify from 'aws-amplify';
+// import { withAuthenticator } from 'aws-amplify-react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import config from './aws-exports';
+// import config from './aws-exports';
 
-Amplify.configure(config);
+// Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
@@ -26,5 +26,5 @@ function App() {
     );
   }
 }
-
-export default withAuthenticator(App);
+export default App;
+// export default withAuthenticator(App);
